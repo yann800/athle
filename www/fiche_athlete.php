6255025370831d6b5072541be0208e2d55ab9c8d
aek <aek@aek-GN735AA-ABF-a6207-fr>
2016-08-03 12:07:00 +0200

    fiche_athlete.php

--- www/fiche_athlete.php
+++ www/fiche_athlete.php
@@ -17,8 +17,31 @@
 
 </head>
 
-
 <body>
+
+
+	<article class="content">
+
+		<div class="col-sm-2"></div>
+
+
+		<div class="col-sm-8">
+
+
+			<nav id="menu"></nav>
+			
+			<div class="alert alert-info info">
+			Pour voir les points, survolez les courbes du diagramme.
+			</div>
+			
+       <div id="chart_div" style="width: 900px; height: 500px;"></div>
+   
+<script type="text/javascript" src="charts/google/jsapi?autoload={'modules':[{'name':'visualization','version':'1','packages':['corechart']}]}"></script>
+   
+<script type="text/javascript">
+   google.setOnLoadCallback(drawChart);
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
 		  
 
 <?php
@@ -71,8 +94,62 @@ else {
 	if ($conn->query($sql) === TRUE) {
 		echo "Record updated successfully";
 	} else {
-		echo "Error updating record: " . $conn->error;
+		echo "Error DB. ";
+		exit;
 	}
+	
+	
+	$sql = "
+
+SELECT annee as annee, max(e_800) as e_800, max(e_1500) as e_1500, max(e_3000) as e_3000
+FROM
+(
+	SELECT annee, 
+		CASE idEpreuve
+			WHEN 800 THEN points
+			WHEN 1500  THEN 0
+			WHEN 3000  THEN 0  
+		END as e_800,
+		
+		CASE idEpreuve
+			WHEN 800 THEN 0
+			WHEN 1500  THEN points
+			WHEN 3000  THEN 0
+		END as e_1500,
+	
+		CASE idEpreuve
+			WHEN 800 THEN 0
+			WHEN 1500 THEN 0
+			WHEN 3000  THEN points
+		END as e_3000
+	
+	FROM ligne l
+	WHERE l.nom = '". $param_nom ."' AND prenom = '". $param_prenom ."' AND l.idEpreuve IN (800, 1500, 3000) AND annee > 2005
+
+) tmp
+GROUP by annee";
+
+
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+    
+        echo "['" . $row["annee"]. "'," . $row["e_800"]. "," . $row["e_1500"]. "," . " $row["e_3000"]. "]";
+
+		if ($row["e_3000"] > 0 and $row["annee"] === 2016){
+			// derniere ligne donc rien
+		}
+		else {
+			echo ",";
+		}
+
+    }
+} else {
+    echo "0 results";
+}
+	
 
 $conn->close();
 
@@ -80,5 +157,75 @@ $conn->close();
 
 ?>
 
+        ]);
+
+        var options = {
+          title: 'Meilleures perfs à la table de cotations par année'
+        };
+
+        var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+
+        chart.draw(data, options);
+      }
+   </script>
+   
+   
+   <br/><br/>
+   		
+			
+
+<pre>
+SELECT annee,max(e_800), max(e_1500), max(e_3000)
+FROM
+(
+	SELECT annee, 
+		CASE idEpreuve
+			WHEN 800 THEN points
+			WHEN 1500  THEN 0
+			WHEN 3000  THEN 0  
+		END as e_800,
+		
+		CASE idEpreuve
+			WHEN 800 THEN 0
+			WHEN 1500  THEN points
+			WHEN 3000  THEN 0
+		END as e_1500,
+	
+		CASE idEpreuve
+			WHEN 800 THEN 0
+			WHEN 1500 THEN 0
+			WHEN 3000  THEN points
+		END as e_3000
+	
+	FROM ligne l
+	WHERE l.nom = '[NOM]' AND prenom = '[PREONOM]' AND l.idEpreuve IN (800, 1500, 3000) AND annee &gt; 2005
+
+) tmp
+GROUP by annee
+</pre>
+
+<p>
+Note : on n'aurait pu faire plus simple si MySql avait implémenté la fonction PIVOT/UNPIVOT (<a href="http://stackoverflow.com/questions/3392956/sql-how-to-transpose">inversion row/col</a>)
+</p>
+
+<pre><samp>
+2010    744    882
+2011    911    933
+2012    984    1090
+2013	806    703
+2014	1547   0
+2015	1343   1297
+2016	1528   683
+</samp>
+</pre>
+		</div>
+	</article>
+
+
+
+	<script src="js/menu.js" type="text/javascript"></script>
+	<script>
+		document.getElementById("recherche").setAttribute("class", "active");
+	</script>
 </body>
 </html>
