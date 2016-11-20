712026b407dfa390fc6a58d6979cd75a54b6e14a
yann800 <yann800@yahoo.fr>
2016-11-20 20:16:52 +0100

    amélioration parseSimpleText

--- src/main/java/fr/yann/parser/ParserSimpleTxt.java
+++ src/main/java/fr/yann/parser/ParserSimpleTxt.java
@@ -53,21 +53,17 @@ public class ParserSimpleTxt {
 		String motAccent = "[A-Za-z\\u00C0\\u00C1\\u00C2\\u00C3\\u00C4\\u00C5\\u00C6\\u00C7\\u00C8\\u00C9\\u00CA\\u00CB\\u00CC\\u00CD\\u00CE\\u00CF\\u00D0\\u00D1\\u00D2\\u00D3\\u00D4\\u00D5\\u00D6\\u00D8\\u00D9\\u00DA\\u00DB\\u00DC\\u00DD\\u00DF\\u00E0\\u00E1\\u00E2\\u00E3\\u00E4\\u00E5\\u00E6\\u00E7\\u00E8\\u00E9\\u00EA\\u00EB\\u00EC\\u00ED\\u00EE\\u00EF\\u00F0\\u00F1\\u00F2\\u00F3\\u00F4\\u00F5\\u00F6\\u00F9\\u00FA\\u00FB\\u00FC\\u00FD\\u00FF\\u0153]+";
 		
 		
-		String rang = "(\\d)+\\s";
+		String rang = "(\\d+)\\s";
 		String perf = "(1'\\d\\d\"\\d\\d)\\s";
 		String nom = "([A-Z]+)\\s";
-		String prenom = "(" + motAccent + ")";
-		String club = "([a-zA-Z\\s]+[a-zA-Z])";
-		String annee = "(\\d\\d)";
-		String strDate = "(\\d{2}/\\d{2}/\\d\\d{4})";
+		String prenom = "(" + motAccent + ")\\s";
+		String club = "([a-zA-Z\\s]+[a-zA-Z])\\s";
+		String annee = "(\\d{2})\\s";
+		String strDate = "(\\d{2}/\\d{2}/\\d{4})\\s";
 		String ville = "(" + motAccent + ")";		
 
-		String pattern = rang + perf 
-				+ nom + prenom 
-				+ club + annee 
-				+ strDate + ville ;
-
-		pattern = rang + perf + nom + "([^\\s]+)\\s([A-Za-z\\s]+)\\s(\\d\\d)\\s(\\d{2}/\\d{2}/\\d{4})\\s(\\w+)";
+		// String pattern = rang + perf + nom + "([^\\s]+)\\s([A-Za-z\\s]+)\\s(\\d\\d)\\s(\\d{2}/\\d{2}/\\d{4})\\s(\\w+)";
+		String pattern = rang + perf + nom + prenom + club + annee + strDate + ville;
 		
 		// Create a Pattern object
 		Pattern r = Pattern.compile(pattern);
@@ -93,7 +89,7 @@ public class ParserSimpleTxt {
 					idEpreuve,
 					sexe);
 
-			sb.append(lb.toStringHtmlTrOfTable());
+			sb.append(lb.toStringHtmlTrOfTable() + "\n");
 		}
 
 	}
