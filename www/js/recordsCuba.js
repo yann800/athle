3858cba75e27462af2e4752fa5d2a86d0b30f949
yann800 <yann800@yahoo.fr>
2017-10-28 19:49:02 +0200

    comparaison : nouveau pays Cuba

---
+++ www/js/recordsCuba.js
@@ -0,0 +1,26 @@
+var recordsCubaHomme =
+{'record' : [
+{e : '100', nom : 'Silvio Leonard', perf :'9.98'},
+{e : '200', nom : 'Roberto Skyers', perf :'20.02'},
+{e : '400', nom : 'Roberto Hernández', perf :'44.14'},
+{e : '800', nom : 'Norberto Téllez', perf :'1.42.85'},
+{e : '1500', nom : 'Maurys Surel Castillo', perf :'3.35.03'},
+{e : '3000', nom : 'Maurys Surel Castillo', perf :'7.58.3'},
+{e : '5000', nom : 'Luis Medina', perf :'13.44.8'},
+{e : '10000', nom : 'Alberto Cuba', perf :'28.48.96'},
+{e : '400 Haies', nom : 'Omar Cisneros', perf :'47.93'},
+{e : 'Hauteur', nom : 'Javier Sotomayor', perf :'2m45'},
+{e : 'Perche', nom : 'Lázaro Borges', perf :'5m90'},
+{e : 'Longueur', nom : 'Iván Pedroso', perf :'8m71'},
+{e : 'Triple saut', nom : 'Pedro Pablo Pichardo', perf :'18m08'}
+]
+};
+
+var recordsCubaFemme =
+{'record' : [
+{e : '100', nom : 'Liliana Alle', perf :'9.98'},
+{e : '200', nom : 'Roxana Díaz', perf :'22.68'},
+{e : '400', nom : 'Ana Fidelia Quirot', perf :'49.61'},
+{e : '800', nom : 'Ana Fidelia Quirot', perf :'1.54.44'}
+]
+};
