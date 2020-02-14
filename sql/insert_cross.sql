4a65b4f4493fd3f2a344b8dd362d7fdce095b95e
yann800 <yann800@yahoo.fr>
2020-02-14 16:45:50 +0100

    page 3 tableau 2020 dep reg lifa

--- sql/insert_cross.sql
+++ sql/insert_cross.sql
@@ -3,3 +3,12 @@
  */
 UPDATE cros set chrono = REPLACE(chrono, '_', '''');
 UPDATE cros set club = REPLACE(club, '_', '''');
+
+SELECT  SUBSTRING(cat, 1,3) FROM cros WHERE LENGTH(cat) > 3;
+
+UPDATE cros SET naissance = SUBSTRING(cat, 5) WHERE naissance = 99;
+UPDATE cros SET cat = SUBSTRING(cat, 1,3) WHERE LENGTH(cat) > 3
+
+
+ALTER TABLE cros DROP xx ;
+
